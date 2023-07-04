10b919ff9602f408a088cd44b6c2e1c01e306048
helloitsminha <minha.mh.lee@gmail.com>
2023-07-04 16:18:30 -0400

    workshopping recipes component, using array to make new API call to spoonacular

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,68 +1,52 @@
-import { useEffect,useState } from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
 
-const Recipes = ({partyAllergies}) => {  
-  console.log(partyAllergies);
-  const healthParams = new URLSearchParams();
-  healthParams.append("type","public");
-  healthParams.append("app_id","162a32d0");
-  healthParams.append("app_key","65ebf6faccbb0d0d696eaefb2708b549");
-  healthParams.append("q",'NOT REQUIRED');
-  console.log(healthParams);
-    // const healthArray = ["dairy-free", "egg-free", "fish-free", "fodmap-free"];
-    if(Array.isArray(partyAllergies)){
-      partyAllergies.forEach((item)=>{
-        healthParams.append("health", item)})
 
-    }
-  
-    
+const Recipes = ({ partyAllergies }) => {
+  console.log(partyAllergies)
+  useEffect(() => {
+    const apiKey = '0364971235b98e447450d193f7298b69'
+    const appId = 'ea09d762'
+    partyAllergies.forEach((partyAllergy) => console.log(partyAllergy));
+    const apiParams = new URLSearchParams ({
+        type: "public",
+        app_id: appId,
+        app_key: apiKey,
 
-    useEffect(()=>{
-      axios({
+    });
 
-        method: 'get',
-        url: ' https://api.edamam.com/api/recipes/v2',
-        params:healthParams    
-      })
-      .then(function (response) {
-      console.log(response.data.hits);
-  });
+    // const healthSearch = partyAllergies.join(" ` " `, health : ` " ` ")
+    // // const healthSearch = partyAllergies.join('", health : "');
+
+    // // const healthSearch = () => {
+    // //     partyAllergies.map((partyAllergy) => apiParams.health=partyAllergy)
+    // // }
+    // console.log(healthSearch)
 
 
 
-    },[])
+    axios({
+      url: 'https://api.edamam.com/api/recipes/v2',
+      method: 'GET',
+      // dataResponse: 'json',
+      params: apiParams
+        
+        // type: "public",
+        // app_id: appId,
+        // app_key: apiKey,
+        // // health: 'gluten-free',
+        // // health: 'vegan',
+        // dishType: 'main course',
+      ,
+    }).then((response) => {
+      console.log(response);
+    })
+  }, []);
   return (
-    <div>Receipes</div>
+    <main>
+      <h1>I am recipes hi</h1>
+    </main>
   )
 }
-Recipes.prototype={
-  partyAllergies:PropTypes.array.isRequired
-}
-export default Recipes
 
-
-    // params:{
-    //     type:"public",
-    //     app_id:"162a32d0",
-    //     app_key:"65ebf6faccbb0d0d696eaefb2708b549",
-    //     q:'NOT REQUIRED',
-        
-    // }
-
-// const url = new URL(`https://api.edamam.com/api/recipes/v2`);
-//  url.search = new URLSearchParams({
-//     type:"public",
-//     app_id:"162a32d0",
-//     app_key:"65ebf6faccbb0d0d696eaefb2708b549",
-//     q:'NOT REQUIRED',
-//     health:"gluten-free",
-//     health:"fish-free"
-    
-//   });
-//   fetch(url)
-//   .then((response)=>{
-//     return response.json()
-//   })
-//   .then(data=> console.log(data.hits))
+export default Recipes;
